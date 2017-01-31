060b720e6017cc1a99a7e7b3c0d07bdc953cf1c0
Richard Royal <richard.royal@coxinc.com>
2017-01-31 09:12:40 -0500

    Added audio to deploy

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,7 @@ cp app/scripts/wNumb.js dist/scripts/
 cp app/scripts/nouislider.js dist/scripts/
 cp app/scripts/audio.js dist/scripts/
 
+mkdir -p dist/audio/
+cp app/audio/* dist/audio/
+
 npm run deploy
